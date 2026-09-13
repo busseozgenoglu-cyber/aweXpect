9f34722989ca401e45e0ea778d01b72e572661f2
busseozgenoglu-cyber <257858631+busseozgenoglu-cyber@users.noreply.github.com>
2026-09-13 12:31:54 +0300

    test: cover control-char expectation

---
+++ Tests/aweXpect.Tests/Chars/ThatChar.IsControl.Tests.cs
@@ -0,0 +1,73 @@
+namespace aweXpect.Tests;
+
+public sealed partial class ThatChar
+{
+	public sealed class IsControl
+	{
+		public sealed class Tests
+		{
+			[Theory]
+			[InlineData('\0')]
+			[InlineData('\t')]
+			[InlineData('\n')]
+			[InlineData('\u007F')]
+			public async Task WhenSubjectIsControl_ShouldSucceed(char subject)
+			{
+				async Task Act()
+					=> await That(subject).IsControl();
+
+				await That(Act).DoesNotThrow();
+			}
+
+			[Theory]
+			[InlineData('a')]
+			[InlineData('5')]
+			[InlineData(' ')]
+			public async Task WhenSubjectIsNotControl_ShouldFail(char subject)
+			{
+				async Task Act()
+					=> await That(subject).IsControl();
+
+				await That(Act).Throws<XunitException>()
+					.WithMessage($"""
+					              Expected that subject
+					              is a control character,
+					              but it was {Formatter.Format(subject)}
+					              """);
+			}
+		}
+
+		public sealed class NegatedTests
+		{
+			[Theory]
+			[InlineData('\0')]
+			[InlineData('\t')]
+			[InlineData('\n')]
+			[InlineData('\u007F')]
+			public async Task WhenSubjectIsControl_ShouldFail(char subject)
+			{
+				async Task Act()
+					=> await That(subject).DoesNotComplyWith(it => it.IsControl());
+
+				await That(Act).Throws<XunitException>()
+					.WithMessage($"""
+					              Expected that subject
+					              is not a control character,
+					              but it was {Formatter.Format(subject)}
+					              """);
+			}
+
+			[Theory]
+			[InlineData('a')]
+			[InlineData('5')]
+			[InlineData(' ')]
+			public async Task WhenSubjectIsNotControl_ShouldSucceed(char subject)
+			{
+				async Task Act()
+					=> await That(subject).DoesNotComplyWith(it => it.IsControl());
+
+				await That(Act).DoesNotThrow();
+			}
+		}
+	}
+}
