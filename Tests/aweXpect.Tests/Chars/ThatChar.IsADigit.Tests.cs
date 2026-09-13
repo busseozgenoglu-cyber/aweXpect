c05f0521977d718774a5455a4d27d02e8c4bacf7
busseozgenoglu-cyber <257858631+busseozgenoglu-cyber@users.noreply.github.com>
2026-09-13 12:29:56 +0300

    test: cover char digit expectation

---
+++ Tests/aweXpect.Tests/Chars/ThatChar.IsADigit.Tests.cs
@@ -0,0 +1,75 @@
+namespace aweXpect.Tests;
+
+public sealed partial class ThatChar
+{
+	public sealed class IsADigit
+	{
+		public sealed class Tests
+		{
+			[Theory]
+			[InlineData('0')]
+			[InlineData('5')]
+			[InlineData('9')]
+			[InlineData('\u0661')]
+			public async Task WhenSubjectIsADigit_ShouldSucceed(char subject)
+			{
+				async Task Act()
+					=> await That(subject).IsADigit();
+
+				await That(Act).DoesNotThrow();
+			}
+
+			[Theory]
+			[InlineData('/')]
+			[InlineData(':')]
+			[InlineData('a')]
+			[InlineData('½')]
+			public async Task WhenSubjectIsNoDigit_ShouldFail(char subject)
+			{
+				async Task Act()
+					=> await That(subject).IsADigit();
+
+				await That(Act).Throws<XunitException>()
+					.WithMessage($"""
+					              Expected that subject
+					              is a digit,
+					              but it was {Formatter.Format(subject)}
+					              """);
+			}
+		}
+
+		public sealed class NegatedTests
+		{
+			[Theory]
+			[InlineData('0')]
+			[InlineData('5')]
+			[InlineData('9')]
+			[InlineData('\u0661')]
+			public async Task WhenSubjectIsADigit_ShouldFail(char subject)
+			{
+				async Task Act()
+					=> await That(subject).DoesNotComplyWith(it => it.IsADigit());
+
+				await That(Act).Throws<XunitException>()
+					.WithMessage($"""
+					              Expected that subject
+					              is not a digit,
+					              but it was {Formatter.Format(subject)}
+					              """);
+			}
+
+			[Theory]
+			[InlineData('/')]
+			[InlineData(':')]
+			[InlineData('a')]
+			[InlineData('½')]
+			public async Task WhenSubjectIsNoDigit_ShouldSucceed(char subject)
+			{
+				async Task Act()
+					=> await That(subject).DoesNotComplyWith(it => it.IsADigit());
+
+				await That(Act).DoesNotThrow();
+			}
+		}
+	}
+}
