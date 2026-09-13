379debe89f2992b0462a87885b4898ecbbd171d7
busseozgenoglu-cyber <257858631+busseozgenoglu-cyber@users.noreply.github.com>
2026-09-13 12:28:49 +0300

    feat: add ASCII hex digit expectation

---
+++ Source/aweXpect/That/Chars/ThatChar.IsAnAsciiHexDigit.cs
@@ -0,0 +1,15 @@
+using aweXpect.SourceGenerators;
+
+namespace aweXpect;
+
+#if NET8_0_OR_GREATER
+[CreateExpectationOn<char>("Is{Not}AnAsciiHexDigit", "char.IsAsciiHexDigit({value})",
+	ExpectationText = "is {not} an ASCII hex digit"
+)]
+#else
+[CreateExpectationOn<char>("Is{Not}AnAsciiHexDigit",
+	"{value} is >= '0' and <= '9' or >= 'a' and <= 'f' or >= 'A' and <= 'F'",
+	ExpectationText = "is {not} an ASCII hex digit"
+)]
+#endif
+public static partial class ThatChar;
