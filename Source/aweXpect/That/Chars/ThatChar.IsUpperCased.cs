35fbedad375020c28f272635abe8c04461f1fcf1
busseozgenoglu-cyber <257858631+busseozgenoglu-cyber@users.noreply.github.com>
2026-09-13 07:36:21 +0300

    feat(chars): add upper-case predicate for #1056

---
+++ Source/aweXpect/That/Chars/ThatChar.IsUpperCased.cs
@@ -0,0 +1,12 @@
+﻿using aweXpect.SourceGenerators;
+
+namespace aweXpect;
+
+[CreateExpectationOn<char>("Is{Not}UpperCased", "char.IsUpper({value})",
+	ExpectationText = "is {not} upper-cased",
+	Remarks = """
+	          This means, that the specified Unicode character is categorized as an uppercase letter.<br />
+	          <seealso cref="char.IsUpper(char)" />
+	          """
+)]
+public static partial class ThatChar;
